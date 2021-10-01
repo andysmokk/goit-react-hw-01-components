efc63263c826859e87a381d4ed956105cd914821
Andrii Konovalov <78738364+andysmokk@users.noreply.github.com>
2021-10-01 12:00:46 +0200

    Updates

---
+++ static/js/main.b522ac9f.chunk.js
@@ -0,0 +1,2 @@
+(this["webpackJsonpgoit-react-hw-01-components"]=this["webpackJsonpgoit-react-hw-01-components"]||[]).push([[0],{11:function(t,n,e){"use strict";e.r(n);var c=e(1),o=e.n(c),r=e(3),s=e.n(r),i=(e(8),e(9),e(0));var a=function(){return Object(i.jsx)("div",{className:"App",children:"HW#1"})};s.a.render(Object(i.jsx)(o.a.StrictMode,{children:Object(i.jsx)(a,{})}),document.getElementById("root"))},8:function(t,n,e){},9:function(t,n,e){}},[[11,1,2]]]);
+//# sourceMappingURL=main.b522ac9f.chunk.js.map
